3882221e225770ad1b4557e57256fb1fe959195e
Rijan <rijan4568@gmail.com>
2025-01-20 21:03:09 +0545

    feat: set api call function

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://c0a6-38-255-151-34.ngrok-free.app";
+const BASE_URL = "https://21da-38-255-151-34.ngrok-free.app";
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
