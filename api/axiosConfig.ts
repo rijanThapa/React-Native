4b36570ea11ccec0acb76cd45d1769a7a0842435
Rijan <rijan4568@gmail.com>
2025-01-22 12:05:09 +0545

    fix: product card

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://21da-38-255-151-34.ngrok-free.app";
+const BASE_URL =
+  "https://e689-2400-1a00-b060-e8df-88b5-2937-cd3f-934.ngrok-free.app";
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
