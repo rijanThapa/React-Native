cca31013ea15bc0f5bdecc3747171e26c38a017c
Rijan <rijan4568@gmail.com>
2025-01-22 21:25:58 +0545

    add add to car button

--- app/product/[productId].tsx
+++ app/product/[productId].tsx
@@ -1,5 +1,6 @@
 import { productSerive } from "@/api/service/product";
 import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Image } from "@/components/ui/image";
@@ -7,7 +8,6 @@ import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import React from "react";
-import { Button, View } from "react-native";
 import { useQuery } from "react-query";
 
 const ProductDetails = () => {
@@ -51,6 +51,22 @@ const ProductDetails = () => {
           </Heading>
           <Text size="sm">{productDetails?.description}</Text>
         </VStack>
+        <Box className="flex-col sm:flex-row">
+          <Button
+            className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1"
+            // onPress={handleAddToCart}
+          >
+            <ButtonText size="sm">Add to cart</ButtonText>
+          </Button>
+          <Button
+            variant="outline"
+            className="px-4 py-2 border-outline-300 sm:flex-1"
+          >
+            <ButtonText size="sm" className="text-typography-600">
+              Wishlist
+            </ButtonText>
+          </Button>
+        </Box>
       </Card>
     </Box>
   );
