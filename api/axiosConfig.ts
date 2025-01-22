f24b65ae2fd3d2a4f00114b40e0a8e2ab0216811
Rijan <rijan4568@gmail.com>
2025-01-22 17:45:43 +0545

    fix: chnage baseUrl

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  "https://e689-2400-1a00-b060-e8df-88b5-2937-cd3f-934.ngrok-free.app";
+  "https://63f6-2400-1a00-b060-e8df-7c61-d07e-ddb2-7864.ngrok-free.app";
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
