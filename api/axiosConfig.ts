315569d0534e0b04b5e7ae35b5bf544293283ee0
Rijan <rijan4568@gmail.com>
2025-01-28 20:21:10 +0545

    chore: chnage base url

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  "https://3dc2-2400-1a00-b060-e8df-d72-b1b9-2c64-c57d.ngrok-free.app";
+const BASE_URL = "https://6651-103-181-227-55.ngrok-free.app";
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
