89262e51b1d70821ddd618b0c2abf244a6bd1fbe
Rijan <rijan4568@gmail.com>
2025-01-30 15:45:22 +0545

    hotfix: base url

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  "https://bbf6-2400-1a00-b060-e8df-f1d9-cf5c-ce1-4dec.ngrok-free.app";
+  "https://843c-2400-1a00-b060-e8df-5df1-26d2-5cfe-8a19.ngrok-free.app";
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
