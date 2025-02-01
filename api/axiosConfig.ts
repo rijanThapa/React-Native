9c8d7a26216b84fadca04de2554202e0dae0bc9e
Rijan <rijan4568@gmail.com>
2025-02-01 21:41:46 +0545

    chore: base url

--- api/axiosConfig.ts
+++ api/axiosConfig.ts
@@ -2,8 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { router } from "expo-router";
 
-const BASE_URL =
-  "https://15aa-2400-1a00-b060-e8df-d0a7-2557-aaa3-a971.ngrok-free.app";
+const BASE_URL = "https://b83a-38-255-148-138.ngrok-free.app";
 
 const getTokenFromStorage = async () => {
   return await AsyncStorage.getItem("token");
@@ -28,18 +27,3 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    if (error.response && error.response.status === 401) {
-      await AsyncStorage.removeItem("token");
-      router.push("/login");
-    }
-    return Promise.reject(error);
-  }
-);
-
-export default api;
